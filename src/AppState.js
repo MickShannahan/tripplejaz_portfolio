b0a14dccdc1850c80cca1c82532193fcfa922d0f
mick shannahan <beasticorn@gmail.com>
2026-02-26 10:17:31 -0700

    linkedin link added

--- src/AppState.js
+++ src/AppState.js
@@ -17,6 +17,7 @@ export const AppState = reactive({
     { text: 'Brushes', link: 'https://tripplejaz.gumroad.com/', icon: 'mdi-brush' },
     { text: 'SkillShare', link: 'https://www.skillshare.com/en/referrals/general?teacherRef=473296912&via=teacher-referral-channel&utm_campaign=teacher-referral-channel&utm_source=ShortUrl&utm_medium=teacher-referral-channel&vanityUsername=tripplejaz', icon: 'mdi-school' },
     { text: 'Resume', link: 'https://drive.google.com/file/d/1Kit57YhMr1oi1T0NaoRsQ_LU9Q-QK1lc/view', icon: 'mdi-file-document-check' },
+    { text: 'LinkedIn', link: 'https://www.linkedin.com/in/joe-zavaletta-817b59123/', icon: 'mdi-linkedin' }
   ],
   siteRoutes: [],
   galleryManifest: null
