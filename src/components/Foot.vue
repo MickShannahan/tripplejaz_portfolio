6dd5c27691c45172b51c5321d7f5b20806ffb7ae
mick shannahan <beasticorn@gmail.com>
2026-03-12 10:25:00 -0600

    updated logo and border

--- src/components/Foot.vue
+++ src/components/Foot.vue
@@ -31,21 +31,27 @@ import SocialLinks from './SocialLinks.vue';
 </template>
 
 <style lang="scss" scoped>
+.bg-black{
+  background-color: #0f0f0f !important;
+}
+
   img{
     width: 200px;
     aspect-ratio: 16/9;
     object-fit: cover;
     object-position: center;
     margin-bottom: -5px;
+    filter: drop-shadow(0px -1px 0px var(--bs-jazzy));
   }
 
   .bg-torn-paper{
-    height: 100px;
+    height: 160px;
     width: 100%;
-    background-image: url(/src/assets/img/splatBorder.png?t=1769052825222), url(/src/assets/img/splatBorder.png?t=1769052825222), url(/src/assets/img/splatBorder.png?t=1769052825222);
-    background-size: 1200px 100%;
+    background-image: url(/src/assets/img/DarkBanner-2026.png), url(/src/assets/img/DarkBanner-2026.png), url(/src/assets/img/DarkBanner-2026.png);
+    background-size: 500px 160%;
+    transform: scale(-1);
     background-repeat: repeat;
-    background-position: -150px;
+    background-position: -150px -20px;
     margin-bottom: -1px;
   }
 
